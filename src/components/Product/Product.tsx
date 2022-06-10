fe1c2efb536aa23b565973cfce7f621853d188c6
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-06-10 08:25:09 -0400

    feat: rediseña la tarjeta de producto

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,10 +1,9 @@
 import { memo } from 'react';
 import { Typography, InputNumber, Avatar } from 'antd';
 import {
-	CaretDownOutlined,
-	CaretUpOutlined,
-	CloseOutlined,
-	FieldTimeOutlined
+	MinusOutlined,
+	DeleteOutlined,
+	PlusOutlined
 } from '@ant-design/icons';
 import { useDispatch } from 'react-redux';
 
@@ -23,49 +22,61 @@ interface ProductProps {
 	id: string;
 	name: string;
 	barcode: string;
+	imageUrl: string;
 	price: number;
 	quantity: number;
 	index: number;
 }
-const Product = ({ id, name, barcode, price, quantity, index }: ProductProps) => {
+const Product = ({ id, name, barcode, price, imageUrl, quantity, index }: ProductProps) => {
 	const dispatch = useDispatch();
 
 	return (
 		<div className="product">
-			<Avatar size={100} icon={<FieldTimeOutlined />} />
+			<div className="content">
+				<Avatar size={100} src={imageUrl} />
 
-			<div className="description">
-				<Title>{name.substr(0, 15)}</Title>
-				<Text>{barcode}</Text>
+				<div className="description">
+					<Title>{name.substr(0, 15)}</Title>
+					<Text>{barcode}</Text>
+				</div>
+				
+				<div className="quantity">
+					<InputNumber
+						type="number"
+						value={quantity}
+						onPressEnter={(event: any) => {
+							let quantity = Number(event.target.value) || 1;
+							dispatch(setQuantity(id, quantity));
+							const barcodeInput: any = document.querySelector('#barcode_input');
+							barcodeInput?.focus()
+						}}
+						className="quantity-input"
+					/>
+					<div className="quantity-controls">
+						<div className="btn" onClick={() => dispatch(increase(id))}>
+							<PlusOutlined style={{ fontSize: 16 }} />
+						</div>
+						<div className="btn"  onClick={() => dispatch(decrease(id))}>
+							<MinusOutlined style={{ fontSize: 16 }} />
+						</div>
+					</div>
+				</div>
+
+				<div className="price">
+					<Text className="price-u">
+						$ {format.cash(price)} /U
+					</Text>
+					<Text className="price-t">
+						$ {format.cash(price * quantity)}
+					</Text>
+				</div>
 			</div>
-			<div className="price">
-				<Text className="price-u">
-					$ {format.cash(price)} /U
-				</Text>
-				<Text className="price-t">
-					$ {format.cash(price * quantity)}
-				</Text>
-			</div>
-			<div className="quantity">
-				<CaretUpOutlined onClick={() => dispatch(increase(id))} />
-				<InputNumber
-					type="number"
-					value={quantity}
-					onPressEnter={(event: any) => {
-						let quantity = Number(event.target.value) || 1;
-						dispatch(setQuantity(id, quantity));
-						const barcodeInput: any = document.querySelector('#barcode_input');
-						barcodeInput?.focus()
-					}}
-					className="quantity-input"
-				/>
-				<CaretDownOutlined onClick={() => dispatch(decrease(id))} />
-			</div>
+
 			<div
 				className="delete"
 				onClick={() => dispatch(removeProductFromCart(index))}
 			>
-				<CloseOutlined />
+				<DeleteOutlined />
 			</div>
 		</div>
 	);
