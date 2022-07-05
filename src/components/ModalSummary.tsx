3a01d317926ae6680a35aefe2c82781fc764f0f5
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-07-04 23:12:46 -0400

    feat: agrega boton para imprimir ticket en modal de resumen

--- src/components/ModalSummary.tsx
+++ src/components/ModalSummary.tsx
@@ -1,16 +1,32 @@
-import { memo } from 'react';
-import { Modal, Row, Typography, Divider } from 'antd';
+import { memo, useRef, useEffect } from 'react';
+import { Modal, Row, Typography, Divider, Button } from 'antd';
+import { PrinterOutlined } from '@ant-design/icons';
 
 import { format } from '../helper';
+import http from '../http';
 
 const { Title, Text } = Typography;
 
 interface ModalSummaryProps {
-	  visible: boolean;
-	  close: () => void;
-	  items: any[];
+	visible: boolean;
+	id: string;
+	close: () => void;
+	items: any[];
 }
-const ModalSummary = ({ visible, close, items }: ModalSummaryProps) => {
+const ModalSummary = ({ visible, close, items, id }: ModalSummaryProps) => {
+	const printBtnRef = useRef<any>(null);
+
+	useEffect(() => {
+		if (visible && printBtnRef.current) {
+			printBtnRef.current?.focus();
+		}
+
+	}, [visible, printBtnRef]);
+
+	function print() {
+		http.post(`/tickets/print/${id}`)
+	}
+
 	return (
 		<Modal
 			visible={visible}
@@ -22,23 +38,37 @@ const ModalSummary = ({ visible, close, items }: ModalSummaryProps) => {
 			className="summary"
 			destroyOnClose
 		>
+			<Row justify='center'>
+				<Button
+					icon={<PrinterOutlined />}
+					ref={printBtnRef}
+					onClick={print}
+					autoFocus
+				>
+					Imprimir
+				</Button>
+			</Row>
 			<Row justify="center">
-				{items.filter(item => !item.main).map((item, index) => 
-					<div className="summary-tile" key={`SI-${index}`}>
-						<Title level={3}>{format.cash(item.amount)}</Title>
-						<Text>{item.title}</Text>
-					</div>
-				)}
+				{items
+					.filter(item => !item.main && item.amount)
+					.map((item, index) => 
+						<div className="summary-tile" key={`SI-${index}`}>
+							<Title level={3} style={{ margin: 0 }}>{format.cash(item.amount)}</Title>
+							<Text style={{ margin: 0 }}>{item.title}</Text>
+						</div>
+					)}
 			</Row>
 			<Divider />
 
 			<Row justify="center">
-				{items.filter(item => item.main).map(item => 
-					<div className={`summary-tile ${item.class}`} key='SIM'>
-						<Title>{format.cash(item.amount)}</Title>
-						<Text>{item.title}</Text>
-					</div>
-				)}
+				{items
+					.filter(item => item.main)
+					.map(item => 
+						<div className={`summary-tile ${item.class}`} key='SIM'>
+							<Title style={{ margin: 0 }}>{format.cash(item.amount)}</Title>
+							<Text style={{ margin: 0 }}>{item.title}</Text>
+						</div>
+					)}
 			</Row>
 		</Modal>
 	);
